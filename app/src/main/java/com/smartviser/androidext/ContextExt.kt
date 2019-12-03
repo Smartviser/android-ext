a0dfc114f05435251c7a3144291aeace17cb44e5
geluard <gil.eluard@gmail.com>
2019-12-03 22:26:00 +0100

    viSer-3835 - Creation of a common library for all applications
    - Added dialer methods

--- app/src/main/java/com/smartviser/androidext/ContextExt.kt
+++ app/src/main/java/com/smartviser/androidext/ContextExt.kt
@@ -8,13 +8,16 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.location.LocationManager
 import android.os.Build
 import android.provider.Telephony
+import android.telecom.TelecomManager
 import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 val Context.telephonyManager: TelephonyManager
     get() = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
@@ -22,6 +25,12 @@ val Context.telephonyManager: TelephonyManager
 val Context.subscriptionManager: SubscriptionManager
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     get() = getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
+
+val Context.locationManager: LocationManager
+    get() = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+val Context.telecomManager: TelecomManager
+    get() = getSystemService(Context.TELECOM_SERVICE) as TelecomManager
 
 // Resources
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -47,6 +56,17 @@ fun Context.isDefaultSmsApp() =
 fun Context.setDefaultSmsApp() {
     val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
     intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
+    startActivity(intent)
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun Context.isDefaultDialerApp() =
+    packageName == telecomManager.defaultDialerPackage
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun Context.setDefaultDialerApp() {
+    val intent = Intent(TelecomManager.ACTION_CHANGE_DEFAULT_DIALER)
+    intent.putExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME, packageName)
     startActivity(intent)
 }
 
